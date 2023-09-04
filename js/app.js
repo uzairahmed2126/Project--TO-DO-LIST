4fd89a4a3c64518f865b5753ff739fbddabfe309
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-09-05 02:00:51 +0530

    list

--- js/app.js
+++ js/app.js
@@ -1,14 +1,25 @@
 const mainContainer = document.querySelector('.main-container')
 const textEl = document.getElementById('text')
 const addListEl = document.getElementById('add-list')
-const addedList = document.querySelector('.added-list')
+const seeListEl = document.getElementById('see-list')
+const addedList = document.getElementById('added-list')
+const faSolid = document.querySelector('.fa-solid')
 
-// textEl.addEventListener('input',(textFun)=>{
-//     textFun.target.value
-// });
-// addListEl.addEventListener('click',listAdd);
-// let listNum = 0
-// function listAdd(){
-//         let text =textEl.value
-//         addedList.innerText=`${''}${listNum++}${text}`
-// }
+textEl.addEventListener('input', (e) => {
+    textVal = e.value
+})
+let CountList = 1;
+addListEl.addEventListener('click', addbtn);
+function addbtn() {
+    let textVal = textEl.value
+    let createElement_div = document.createElement('div')
+    let createElement_h1 = document.createElement('h1')
+    createElement_h1.style.color = 'yellow'
+    faSolid.style.visibility = 'visible'
+    mainContainer.appendChild(createElement_div)
+    createElement_div.appendChild(createElement_h1)
+    createElement_h1.innerText = `${CountList++}${'.'}${textVal}`
+    // faSolid.addEventListener('click', () => {
+    //     createElement_h1.innerText = `${CountList--}${'.'}${textVal=''}`
+    // })
+}
