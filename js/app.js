077b42e497aab45402e7995ec70fc5927a99682f
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-09-04 23:52:28 +0530

    to do list

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const mainContainer = document.querySelector('.main-container')
+const textEl = document.getElementById('text')
+const addListEl = document.getElementById('add-list')
+const addedList = document.querySelector('.added-list')
+
+// textEl.addEventListener('input',(textFun)=>{
+//     textFun.target.value
+// });
+// addListEl.addEventListener('click',listAdd);
+// let listNum = 0
+// function listAdd(){
+//         let text =textEl.value
+//         addedList.innerText=`${''}${listNum++}${text}`
+// }
