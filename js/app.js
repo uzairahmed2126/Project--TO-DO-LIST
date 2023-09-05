2ede9c9d04a52c421329472c3b78d6190a98f3af
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-09-05 19:49:52 +0530

    to do list

--- js/app.js
+++ js/app.js
@@ -11,15 +11,45 @@ textEl.addEventListener('input', (e) => {
 let CountList = 1;
 addListEl.addEventListener('click', addbtn);
 function addbtn() {
-    let textVal = textEl.value
-    let createElement_div = document.createElement('div')
-    let createElement_h1 = document.createElement('h1')
-    createElement_h1.style.color = 'yellow'
-    faSolid.style.visibility = 'visible'
-    mainContainer.appendChild(createElement_div)
-    createElement_div.appendChild(createElement_h1)
-    createElement_h1.innerText = `${CountList++}${'.'}${textVal}`
-    // faSolid.addEventListener('click', () => {
-    //     createElement_h1.innerText = `${CountList--}${'.'}${textVal=''}`
-    // })
+    if (textEl.value == '') {
+        alert('first write down')
+    }
+    else {
+        createElement_h1.innerText = `${CountList = ''}${'.'}${textVal}`
+
+    }
+    if (textEl.value) {
+        let textVal = textEl.value
+        let createElement_div = document.createElement('div')
+        let createElement_h1 = document.createElement('ul')
+        createElement_h1.style.color = 'yellow'
+        mainContainer.appendChild(createElement_div)
+        createElement_div.appendChild(createElement_h1)
+        createElement_h1.innerText = `${CountList++}${'.'}${textVal}`
+    }
+    if (textEl.value != '') {
+        textEl.value = ''
+    }
+    if (window == 'Enter') {
+        alert('ad')
+    }
 }
+seeListEl.addEventListener('click', () => {
+    if (textEl.value == '') {
+        alert('first write down')
+    }
+    if (textEl.value) {
+        document.body.style.background = '#000'
+        let createElement_div = document.createElement('div')
+        let createElement_table = document.createElement('table')
+        let createElement_list = document.createElement('th')
+        createElement_div.className = 'list-div'
+        createElement_table.className = 'list-table'
+        createElement_list.className = 'list-ul'
+        document.body.appendChild(createElement_div)
+        createElement_div.appendChild(createElement_table)
+        createElement_table.appendChild(createElement_list)
+    }
+
+})
+
